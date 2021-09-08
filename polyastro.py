4797499b6101959b887275ea0abcb1d4a857cc89
edygordo <aditya.sahu.96@gmail.com>
2021-09-08 22:45:53 +0530

    Capgemini Remove Duplicate
    Test questions

---
+++ polyastro.py
@@ -0,0 +1,21 @@
+array1 = dict()
+array2 = dict()
+
+x = int(input())
+y = int(input())
+
+for _ in range(x):
+    temp = int(input())
+    array1[temp] = 1
+
+for _ in range(y):
+    temp = int(input())
+    array2[temp] = 1
+
+result_arr = []
+for key in array1:
+    result_arr.append(key)
+for key in array2:
+    result_arr.append(key)
+result_arr.sort()
+print(result_arr[int(len(result_arr)/2)])
